a6594593eaecfb88531fd24e47ff7a67839c233a
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2021-01-13 23:42:19 +0100

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,30 @@
+var urlsToCache = [
+  '/'
+  //'/styles/styles.css',
+  //'/script/home.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache')
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // The responce is in the cache
+        if (response) {
+          return response;
+        }
+
+        // No cache match, we attempt to fetch it from the network
+        return fetch(event.request);
+      }
+    )
+  );
+});
