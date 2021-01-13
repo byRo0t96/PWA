2375d57d8f201978aec76356c0c77abe6bc0e0df
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2021-01-14 00:43:46 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 window.onload = () => {
   'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
 }
+}
+
